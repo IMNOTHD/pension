afcf3ee813fbed23c9a1a79b81a5b9d06ed7c87b
MardingCobe <51356536+MardingCobe@users.noreply.github.com>
2020-07-19 13:14:05 +0800

    homepage\cell的添加

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -1,56 +1,61 @@
 <template>
     <div class="item-container">
         <list ref="scroller">
-            <!--搜索框、定位以及信息提示服务-->
-            <div class="top-function" style="width: 720px;display: flex; flex-wrap: wrap; flex-direction: row;margin-top: 20px">
-                <div class="top-function-location" style="width: 80px;">
-                    <!--定位-->
-                    <location></location>
+            <cell>
+                <!--搜索框、定位以及信息提示服务-->
+                <div class="top-function" style="width: 720px;display: flex; flex-wrap: wrap; flex-direction: row;margin-top: 20px">
+                    <div class="top-function-location" style="width: 80px;">
+                        <!--定位-->
+                        <location></location>
+                    </div>
+                    <div class="top-function-search" style="width: 580px;">
+                        <!--搜索框-->
+                        <search style="width: 580px;"></search>
+                    </div>
+                    <div class="top-function-information" >
+                        <!--消息-->
+                        <img src="https://i.loli.net/2020/07/19/MfYz2JC8uFeVQ4L.png" style="width: 50px; height: 50px">
+                    </div>
                 </div>
-                <div class="top-function-search" style="width: 580px;">
-                    <!--搜索框-->
-                    <search style="width: 580px;"></search>
+            </cell>
+            <cell>
+                <!--        FirstFuntion-->
+                <div class="first-function">
+                    <img src="https://i.loli.net/2020/07/19/fFmA2GXv9r7dioP.png" style="width: 150px ;height:180px;margin-left: 60px" />
+                    <img src="https://i.loli.net/2020/07/19/AGUzCfrm2WBiI9R.png" style="width: 150px ;height:180px;margin-left: 80px" />
+                    <img src="https://i.loli.net/2020/07/19/Ft76PA5RDILWj3d.png" style="width: 150px ;height:180px;margin-left: 80px" />
                 </div>
-                <div class="top-function-information" >
-                    <!--消息-->
-                    <img src="https://i.loli.net/2020/07/19/MfYz2JC8uFeVQ4L.png" style="width: 50px; height: 50px">
+                <div class="separator"></div>
+            </cell>
+            <cell>
+                <!--SecondFunction-->
+                <div class="second-function" >
+                    <div class="second-function-detail">
+                        <img src="https://i.loli.net/2020/07/19/cCMdhF1SeraHxkW.png" style="width: 120px;height: 120px">
+                        <text>惠民政策</text>
+                    </div>
+                    <div class="second-function-detail">
+                        <img src="https://i.loli.net/2020/07/19/nh5FugobmJyAVfR.png" style="width: 120px;height: 120px">
+                        <text>附近圈子</text>
+                    </div>
+                    <div class="second-function-detail">
+                        <img src="https://i.loli.net/2020/07/19/iDF5fVEne7BL6d9.png" style="width: 120px;height: 120px">
+                        <text>调查问卷</text>
+                    </div>
+                    <div class="second-function-detail">
+                        <img src="https://i.loli.net/2020/07/19/2mMvnocH8Ou4ZyW.png" style="width: 120px;height: 120px">
+                        <text>社区活动</text>
+                    </div>
                 </div>
-            </div>
-            <!--        FirstFuntion-->
-            <div class="first-function">
-                <img src="https://i.loli.net/2020/07/19/fFmA2GXv9r7dioP.png" style="width: 150px ;height:180px;margin-left: 60px" />
-                <img src="https://i.loli.net/2020/07/19/AGUzCfrm2WBiI9R.png" style="width: 150px ;height:180px;margin-left: 80px" />
-                <img src="https://i.loli.net/2020/07/19/Ft76PA5RDILWj3d.png" style="width: 150px ;height:180px;margin-left: 80px" />
-            </div>
+            </cell>
+            <cell>
+                <div class="separator"></div>
 
-            <div class="separator"></div>
-
-            <!--SecondFunction-->
-            <div class="second-function" >
-                <div class="second-function-detail">
-                    <img src="https://i.loli.net/2020/07/19/cCMdhF1SeraHxkW.png" style="width: 120px;height: 120px">
-                    <text>惠民政策</text>
+                <!--绑定老人、匹配圈子功能-->
+                <div class="item-container" :style="contentStyle">
+                    <img src="https://i.loli.net/2020/07/19/lHEyea8buBcI1Ko.png" style="width: 500px;height: 500px;margin-top: 20px">
                 </div>
-                <div class="second-function-detail">
-                    <img src="https://i.loli.net/2020/07/19/nh5FugobmJyAVfR.png" style="width: 120px;height: 120px">
-                    <text>附近圈子</text>
-                </div>
-                <div class="second-function-detail">
-                    <img src="https://i.loli.net/2020/07/19/iDF5fVEne7BL6d9.png" style="width: 120px;height: 120px">
-                    <text>调查问卷</text>
-                </div>
-                <div class="second-function-detail">
-                    <img src="https://i.loli.net/2020/07/19/2mMvnocH8Ou4ZyW.png" style="width: 120px;height: 120px">
-                    <text>社区活动</text>
-                </div>
-            </div>
-
-            <div class="separator"></div>
-
-            <!--绑定老人、匹配圈子功能-->
-            <div class="item-container" :style="contentStyle">
-                <img src="https://i.loli.net/2020/07/19/lHEyea8buBcI1Ko.png" style="width: 500px;height: 500px;margin-top: 20px">
-            </div>
+            </cell>
         </list>
     </div>
 </template>

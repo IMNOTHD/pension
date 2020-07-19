c7318c7e0c3b3594d1e9e5f451426df363bd1a7a
MardingCobe <51356536+MardingCobe@users.noreply.github.com>
2020-07-19 13:07:33 +0800

    scroller功能的添加

--- src/components/Discovery/Discovery.vue
+++ src/components/Discovery/Discovery.vue
@@ -1,17 +1,21 @@
 \
 <template>
-    <div class="container">
-        <div class="container-detail">
-            <text class="container-title">救助</text>
-        </div>
+    <list ref="scroller">
+        <cell>
+            <div class="container">
+                <div class="container-detail">
+                    <text class="container-title">救助</text>
+                </div>
 
-        <div class="separator"></div>
+                <div class="separator"></div>
 
-        <div class="container-detail" style="height: 1000px">
-            <img class="container-help-image" src="https://i.loli.net/2020/07/19/aGIZ5vw3y8R1OmH.png">
-        </div>
+                <div class="container-detail" style="height: 1000px">
+                    <img class="container-help-image" src="https://i.loli.net/2020/07/19/aGIZ5vw3y8R1OmH.png">
+                </div>
 
-    </div>
+            </div>
+        </cell>
+    </list>
 </template>
 
 <script>
--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -1,56 +1,57 @@
 <template>
     <div class="item-container">
-        <!--搜索框、定位以及信息提示服务-->
-        <div class="top-function" style="width: 720px;display: flex; flex-wrap: wrap; flex-direction: row;margin-top: 20px">
-            <div class="top-function-location" style="width: 80px;">
-                <!--定位-->
-                <location></location>
+        <list ref="scroller">
+            <!--搜索框、定位以及信息提示服务-->
+            <div class="top-function" style="width: 720px;display: flex; flex-wrap: wrap; flex-direction: row;margin-top: 20px">
+                <div class="top-function-location" style="width: 80px;">
+                    <!--定位-->
+                    <location></location>
+                </div>
+                <div class="top-function-search" style="width: 580px;">
+                    <!--搜索框-->
+                    <search style="width: 580px;"></search>
+                </div>
+                <div class="top-function-information" >
+                    <!--消息-->
+                    <img src="https://i.loli.net/2020/07/19/MfYz2JC8uFeVQ4L.png" style="width: 50px; height: 50px">
+                </div>
             </div>
-            <div class="top-function-search" style="width: 580px;">
-                <!--搜索框-->
-                <search style="width: 580px;"></search>
+            <!--        FirstFuntion-->
+            <div class="first-function">
+                <img src="https://i.loli.net/2020/07/19/fFmA2GXv9r7dioP.png" style="width: 150px ;height:180px;margin-left: 60px" />
+                <img src="https://i.loli.net/2020/07/19/AGUzCfrm2WBiI9R.png" style="width: 150px ;height:180px;margin-left: 80px" />
+                <img src="https://i.loli.net/2020/07/19/Ft76PA5RDILWj3d.png" style="width: 150px ;height:180px;margin-left: 80px" />
             </div>
-            <div class="top-function-information" >
-                <!--消息-->
-                <img src="https://i.loli.net/2020/07/19/MfYz2JC8uFeVQ4L.png" style="width: 50px; height: 50px">
-            </div>
-        </div>
-<!--        FirstFuntion-->
-        <div class="first-function">
-            <img src="https://i.loli.net/2020/07/19/fFmA2GXv9r7dioP.png" style="width: 150px ;height:180px;margin-left: 60px" />
-            <img src="https://i.loli.net/2020/07/19/AGUzCfrm2WBiI9R.png" style="width: 150px ;height:180px;margin-left: 80px" />
-            <img src="https://i.loli.net/2020/07/19/Ft76PA5RDILWj3d.png" style="width: 150px ;height:180px;margin-left: 80px" />
-        </div>
 
-        <div class="separator"></div>
+            <div class="separator"></div>
 
-        <!--SecondFunction-->
-        <div class="second-function" >
-            <div class="second-function-detail">
-                <img src="https://i.loli.net/2020/07/19/cCMdhF1SeraHxkW.png" style="width: 120px;height: 120px">
-                <text>惠民政策</text>
+            <!--SecondFunction-->
+            <div class="second-function" >
+                <div class="second-function-detail">
+                    <img src="https://i.loli.net/2020/07/19/cCMdhF1SeraHxkW.png" style="width: 120px;height: 120px">
+                    <text>惠民政策</text>
+                </div>
+                <div class="second-function-detail">
+                    <img src="https://i.loli.net/2020/07/19/nh5FugobmJyAVfR.png" style="width: 120px;height: 120px">
+                    <text>附近圈子</text>
+                </div>
+                <div class="second-function-detail">
+                    <img src="https://i.loli.net/2020/07/19/iDF5fVEne7BL6d9.png" style="width: 120px;height: 120px">
+                    <text>调查问卷</text>
+                </div>
+                <div class="second-function-detail">
+                    <img src="https://i.loli.net/2020/07/19/2mMvnocH8Ou4ZyW.png" style="width: 120px;height: 120px">
+                    <text>社区活动</text>
+                </div>
             </div>
-            <div class="second-function-detail">
-                <img src="https://i.loli.net/2020/07/19/nh5FugobmJyAVfR.png" style="width: 120px;height: 120px">
-                <text>附近圈子</text>
-            </div>
-            <div class="second-function-detail">
-                <img src="https://i.loli.net/2020/07/19/iDF5fVEne7BL6d9.png" style="width: 120px;height: 120px">
-                <text>调查问卷</text>
-            </div>
-            <div class="second-function-detail">
-                <img src="https://i.loli.net/2020/07/19/2mMvnocH8Ou4ZyW.png" style="width: 120px;height: 120px">
-                <text>社区活动</text>
-            </div>
-        </div>
 
-        <div class="separator"></div>
+            <div class="separator"></div>
 
-        <!--绑定老人、匹配圈子功能-->
-        <div class="item-container" :style="contentStyle">
-            <img src="https://i.loli.net/2020/07/19/lHEyea8buBcI1Ko.png" style="width: 500px;height: 500px;margin-top: 20px">
-        </div>
-
+            <!--绑定老人、匹配圈子功能-->
+            <div class="item-container" :style="contentStyle">
+                <img src="https://i.loli.net/2020/07/19/lHEyea8buBcI1Ko.png" style="width: 500px;height: 500px;margin-top: 20px">
+            </div>
+        </list>
     </div>
 </template>
 <script>
@@ -69,6 +70,7 @@ export default {
 <style scoped>
     .item-container{
         background-color: white;
+        align-items: center;
     }
     .first-function{
         width: 750px;
@@ -84,7 +86,7 @@ export default {
         margin-top: 80px;
     }
     .second-function-detail{
-        margin-left: 50px
+        margin-left: 50px;
     }
     .separator{
         background-color: #F5F5F5;
